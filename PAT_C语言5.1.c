37c46e59abb335a048b71e83ec7c85e8e9616b19
zhangtaigui <196162153@qq.com>
2022-04-16 00:56:04 +0800

    5.

---
+++ PAT_C语言5.1.c
@@ -0,0 +1,8 @@
+int sum(int m, int n){
+    int i;
+    int tep;
+    for(i=m;i<=n;i++){
+    tep+=i;
+    }
+    return tep;
+}
---
+++ PAT_C语言5.2.c
@@ -0,0 +1,5 @@
+int max( int a, int b ){
+    if(a<b)
+        return b;
+    else return a;
+}
